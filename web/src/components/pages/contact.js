6fbf6c6f3c687d05e86ec1e62c7686d6a4eec7f5
Yuhe Zhu <yuhe.zhu@richmond.edu>
2019-01-17 16:54:36 -0500

    Contact.js update

--- web/src/components/pages/contact.js
+++ web/src/components/pages/contact.js
@@ -12,9 +12,31 @@ class Contact extends Component {
                 </h1>
 
                 <p>
-                    email
+                    We appreciate your interest! Please reach out to us if you would like to: Propose an activity or collaboration; Comment on a previous activity; Or make a general inquiry.
 
                 </p>
+
+                <p>Richmond, VA
+                </p>
+                <p>cssa@richmond.edu
+                </p>
+
+                Your Name
+                <br />
+                <input name="realname" />
+                <br />
+                Your Email
+                <br />
+                <input name="email" />
+                <br />
+                Your Message
+                <br />
+                <textarea cols="40" rows="10" name="Message"></textarea>
+                <br />
+                <input type="submit" value="Send" />
+                <input type="hidden" name="recipient" value="cssa@richmond.edu" />
+                <input type="hidden" name="subject" value="Subject" />
+                <input type="hidden" name="required" value="realname,email,Message" />
 
 
 
