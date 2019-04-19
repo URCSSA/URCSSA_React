50c24e59bc888c25c6999d1dbc697ef815b0d48c
Yuhe Zhu <yuhe.zhu@richmond.edu>
2019-04-19 17:36:39 -0400

    import stack grid

--- web/src/components/pages/homePage.js
+++ web/src/components/pages/homePage.js
@@ -9,50 +9,111 @@ import voiceofva from '../../Assets/images/devResources_OriginalWeb/voiceofva.jp
 import temp from '../../Assets/images/devResources_OriginalWeb/indexTemp.jpg';
 import {Link} from "react-router-dom";
 import { Layout, Header, Navigation, Drawer, Content, Grid, Cell } from 'react-mdl';
-
+import StackGrid, { transitions }  from "react-stack-grid";
+import sizeMe from 'react-sizeme';
+const { scaleDown } = transitions;
 
 
 class Homepage extends Component {
     render() {
+        const { 
+            size: { 
+              width
+            } 
+          } = this.props;
+
         return (
             <homepage>
             <div style={{width: '100%', margin: 'auto'}}>
-            <Grid className="landing-grid">
-            <Cell col={12}>
-            </Cell>
-            <div className="majorBanner">
-            <img className="bannerImg" src={homeBanner} class="img-fluid" alt="banner"/>
-            <div className="bannerContent">
-            <div className="large">
-            CHINESE STUDENTS AND SCHOLARS ASSOCIATION
-        </div>
-        <div className="small">University of Richmond </div>
-        </div>
-        </div>
-            </Grid>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                    <div className="majorBanner">
+                        <img className="bannerImg" 
+                            src={homeBanner} class="img-fluid" 
+                            alt="banner"
+                            />
+                        <div className="bannerContent">
+                            <div className="large">
+                                <h1>CHINESE STUDENTS AND SCHOLARS ASSOCIATION</h1>
+                                </div>
+                            <div className="small">
+                                <h1>University of Richmond </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
 
-                // <div className="container-fluid">
+                 <div className="container-fluid">
 
-                // </div>
+                 </div>
 
-        <Grid className="demo-grid-ruler">
-            <Cell col={4}>
-                <Link to="/About"><img className="galleryImg" src={ourTeam16} class="img-circle img-responsive" alt="team"/>
+        {/* <Grid className="demo-grid-ruler">
+            <Cell col={3}>
+                <Link to="/About">
+                    <img className="galleryImg" 
+                        src={ourTeam16} 
+                        class="img-circle img-responsive" 
+                        alt="team"
+                        />
                     </Link>
             </Cell>
+            <Cell col={2}>
+                <Link to="/">
+                    <img className="galleryImg" 
+                        src={site16} 
+                        class="img-circle img-responsive"  
+                        alt="site"
+                        />
+                </Link>
+                </Cell>
             <Cell col={4}>
-                <Link to="/"><img className="galleryImg" src={site16} class="img-circle img-responsive"  alt="site"/></Link>
-            </Cell>
-            <Cell col={4}>
-                <Link to="Information"><img className="galleryImg" src={handBook} class="img-circle img-responsive" alt="handbook"/></Link>
-            </Cell>
-            </Grid>
+                <Link to="Information">
+                    <img className="galleryImg" 
+                        src={handBook} 
+                        class="img-circle img-responsive" 
+                        alt="handbook"
+                        />
+                    </Link>
+                </Cell>
+            </Grid> */}
+        <StackGrid 
+            appear={scaleDown.appear}
+            appeared={scaleDown.appeared}
+            enter={scaleDown.enter}
+            entered={scaleDown.entered}
+            leaved={scaleDown.leaved}
+            columnWidth={width <= 768 ? '100%' : '33.33%'}>
+            {/* <Link to="/About">
+                    <img className="galleryImg" 
+                        src={ourTeam16} 
+                        class="img-circle img-responsive" 
+                        alt="team"
+                        />
+                </Link> */}
+            
+            <Link to="/">
+                <img className="galleryImg" 
+                    src={site16} 
+                    class="img-circle img-responsive"  
+                    alt="site"
+                    />
+            </Link>
 
+            <Link to="Information">
+                <img className="galleryImg" 
+                    src={handBook} 
+                    class="img-circle img-responsive" 
+                    alt="handbook"
+                    />
+                </Link>
+        </StackGrid>
 
             </homepage>
     );
     }
 }
 
-export default Homepage;
+//export default Homepage;
+export default sizeMe()(Homepage);
